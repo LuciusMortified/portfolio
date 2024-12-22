38d062dd44a78a72402a7dd4e579060929fdff4d
Leonid Fedorov <l.fedorov@2gis.ru>
2024-12-22 15:08:54 +0300

    initial commit

---
+++ app/components/nav.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ThemeSwitch } from "./theme-switch";
+
+const navItems = {
+  "/": { name: "Обо мне" },
+  "/blog": { name: "Блог" },
+  "/projects": { name: "Проекты" },
+  "/photos": { name: "Фото" },
+};
+
+export function Navbar() {
+  const path = usePathname();
+
+  return (
+    <nav className="lg:mb-16 mb-12 py-5">
+      <div className="flex flex-col md:flex-row md:items-center justify-between">
+        <div className="flex"></div>
+        <div className="flex flex-row gap-4 mt-6 md:mt-0 md:ml-auto items-center">
+          {Object.entries(navItems).map(([itemPath, { name }]) => (
+            <Link
+              key={itemPath}
+              href={itemPath}
+              className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative ${itemPath === path ? 'underline' : ''}`}
+            >
+              {name}
+            </Link>
+          ))}
+          <ThemeSwitch />
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ app/config.ts
@@ -0,0 +1,15 @@
+export const metaData = {
+  baseUrl: "https://nextfolio-template.vercel.app/",
+  title: "Федоров Леонид",
+  ogImage: "/opengraph-image.png",
+  name: "Федоров Леонид",
+  description:
+    "Сайт-портфолио бэкенд программиста Федорова Леонида.",
+};
+
+export const socialLinks = {
+  vk: "https://vk.com/luciusmortified",
+  github: "https://github.com/LuciusMortified",
+  telegram: "https://t.me/z1lt0id",
+  email: "mailto:leo.fed97@gmail.com",
+};
